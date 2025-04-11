5f0d07225c8b81fb29c3ea4689b07d66d5c33155
shenchenyu02 <shenchenyu02@meituan.com>
2025-04-11 18:11:14 +0800

    apollo 配置

--- ddd-engineer-domain/src/main/java/com/codingshen/demo/domain/service/DemoDomainService.java
+++ ddd-engineer-domain/src/main/java/com/codingshen/demo/domain/service/DemoDomainService.java
@@ -1,18 +1,26 @@
 package com.codingshen.demo.domain.service;
 
+import com.codingshen.demo.domain.configuration.IConfiguration;
 import com.codingshen.demo.domain.model.entity.Demo;
 import com.codingshen.demo.domain.repositoy.IDemoRepository;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@Slf4j
 public class DemoDomainService {
     @Resource
     private IDemoRepository demoRepository;
 
+    @Resource
+    private IConfiguration apolloConfiguration;
+
     public List<Demo> list() {
+        String testValue = apolloConfiguration.getTestValue();
+        log.info("testValue: {}", testValue);
         return demoRepository.list();
     }
 }
